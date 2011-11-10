4a10f82675b172638b9a402dca0c1323d4314811
Riku Salminen <riku.salminen@tkk.fi>
2011-11-10 20:01:19 +0200

    Keyboard piano

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 #include <assert.h>
 
 #include <SDL/SDL.h>
@@ -8,10 +9,29 @@
 #include <synth.h>
 #include <audio_output.h>
 
+static float note_freq(int i)
+{
+    const float A = 440.0;
+    return (A / 32.0) * pow(2.0, (i - 9) / 12.0);
+}
+
 static int main_loop(synth_t *synth)
 {
-    bool quit = false;
+    const SDLKey keyboard[] = {
+    //  c       c#      d       d#      e       f       f#      g       g#      a       a#      b       c       d       e
+        SDLK_z, SDLK_s, SDLK_x, SDLK_d, SDLK_c, SDLK_v, SDLK_g, SDLK_b, SDLK_h, SDLK_n, SDLK_j, SDLK_m, SDLK_COMMA, SDLK_l, SDLK_PERIOD,
+        SDLK_q, SDLK_2, SDLK_w, SDLK_3, SDLK_e, SDLK_r, SDLK_5, SDLK_t, SDLK_6, SDLK_y, SDLK_7, SDLK_u, SDLK_i, SDLK_9, SDLK_o, SDLK_0, SDLK_p
+    };
+    int notes[] = {
+    //  c       c#      d       d#      e       f       f#      g       g#      a       a#      b       c       d       e
+        0,      1,      2,      3,      4,      5,      6,      7,      8,      9,      10,     11,     12,     13,     14,
+        12,    13,     14,     15,     16,     17,     18,     19,     20,     21,      22,     23,     24,     25,     26,     27,     28,
+    };
 
+    int num_keys = sizeof(keyboard)/sizeof(*keyboard);
+    int base_octave = 4;
+
+    bool quit = false;
     while(!quit)
     {
         const float background[] = { 0.2, 0.4, 0.7, 1.0 };
@@ -27,10 +47,15 @@ static int main_loop(synth_t *synth)
                 quit = true;
             else if(event.type == SDL_KEYDOWN)
             {
-                SDL_LockAudio();
-                for(int i = 0; i < 3; ++i)
-                    adsr_trigger(&synth->instruments[i].adsr);
-                SDL_UnlockAudio();
+                for(int i = 0; i < num_keys; ++i)
+                {
+                    if(keyboard[i] != event.key.keysym.sym) continue;
+
+                    SDL_LockAudio();
+                    synth->instruments->carrier.freq = note_freq(base_octave * 12 + notes[i]);
+                    adsr_trigger(&synth->instruments[0].adsr);
+                    SDL_UnlockAudio();
+                }
             }
         }
     }
--- src/synth.c
+++ src/synth.c
@@ -3,12 +3,6 @@
 
 #include <synth.h>
 
-static float note_freq(int i)
-{
-    const float A = 440.0;
-    return (A / 32.0) * pow(2.0, (i - 9) / 12.0);
-}
-
 int synth_init(synth_t *synth, int sample_rate)
 {
     memset(synth, 0, sizeof(synth_t));
@@ -20,19 +14,15 @@ int synth_init(synth_t *synth, int sample_rate)
         ++instrument)
     {
         instrument->modulation = MODULATION_FREQUENCY;
-        instrument->carrier.waveform = OSCILLATOR_SINE;
-        instrument->carrier.amplitude = 0.3;
-        instrument->modulator.waveform = OSCILLATOR_SQUARE;
+        instrument->carrier.waveform = OSCILLATOR_TRIANGLE;
+        instrument->carrier.amplitude = 0.7;
+        instrument->modulator.waveform = OSCILLATOR_SINE;
         instrument->modulator.freq = 2.0;
         instrument->modulator.amplitude = 0.6;
 
         adsr_set(&instrument->adsr, sample_rate, 0.1, 1.0, 0.7, 1.5);
-        filter_set(&instrument->filter, sample_rate, FILTER_NONE, 500, 0.5, 0.5);
+        filter_set(&instrument->filter, sample_rate, FILTER_LOWPASS, 500, 0.5, 0.5);
     }
-
-    synth->instruments[0].carrier.freq = note_freq(60);
-    synth->instruments[1].carrier.freq = note_freq(64);
-    synth->instruments[2].carrier.freq = note_freq(67);
 
     return 0;
 }
